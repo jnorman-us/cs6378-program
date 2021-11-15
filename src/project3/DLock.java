6970b26954d98e55fd46be69946141948c1b0bfe
josephnormandev <jnormantransactions@gmail.com>
2021-11-15 13:08:37 -0600

    creates the boiler plate code for running the critical section

---
+++ src/project3/DLock.java
@@ -0,0 +1,38 @@
+package project3;
+
+import node.Listener;
+import node.Message;
+import node.Node;
+import node.NodeID;
+
+public class DLock implements Listener {
+    private Node node;
+
+    private NodeID id;
+    private String configFile;
+
+    public DLock(NodeID id, String configFile) {
+        this.id = id;
+        this.configFile = configFile;
+
+        this.node = new Node(id, configFile, this);
+    }
+
+    public void lock() {
+
+    }
+
+    public void unlock() {
+
+    }
+
+    @Override
+    public void receive(Message message) {
+
+    }
+
+    @Override
+    public void broken(NodeID neighbor) {
+
+    }
+}
